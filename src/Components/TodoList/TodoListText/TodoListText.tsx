a1a9cf54c0e276b6c9354df58cbc6e76f10aa81a
tomered <125219614+tomered@users.noreply.github.com>
2023-12-16 22:49:41 +0200

    implement todo list text component (#5)
    * implement todo list text component
    
    * refactor todo list text component

---
+++ src/Components/TodoList/TodoListText/TodoListText.tsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import React, { useRef, useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { COLORS } from "../../../colors";
+
+export default function TodoListText() {
+  const [description, setDescription] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+  };
+  return (
+    <Box sx={{ display: "flex", width: "30vw" }}>
+      <FormControl sx={{ width: "100%", marginRight: 2 }}>
+        <OutlinedInput
+          placeholder="Add something to do.."
+          value={description}
+          type="text"
+          id="message"
+          name="message"
+          onChange={handleChange}
+        />
+      </FormControl>
+      <Button
+        sx={{
+          bgcolor: COLORS.ADD_BUTTON_BG,
+          color: COLORS.ADD_BUTTON,
+          borderRadius: 1,
+          "&:hover": {
+            backgroundColor: COLORS.ADD_BUTTON_HOVER,
+          },
+        }}
+        aria-label="add"
+        onClick={() => {
+          console.log(description);
+          setDescription("");
+        }}
+      >
+        <AddIcon />
+      </Button>
+    </Box>
+  );
+}
