f52d4d6ea70ad2bd62d5b62a48b398b0359fec44
Tomer Edelsberg <tomeredel@gmail.com>
2023-12-16 20:13:48 +0200

    implement todo list text component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import TodoListPage from "./pages/TodoListPage/TodoListPage";
+import TodoListText from "./Components/TodoList/TodoListText/TodoListText";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <TodoListPage />
+      {/* <TodoListPage /> */}
+      <TodoListText/>
     </>
   );
 }
--- src/colors.ts
+++ src/colors.ts
@@ -1,4 +1,7 @@
 export const COLORS = {
     RED_BUTTON:'red',
-    RED_BUTTON_HOVER: '#8c1c03'
+    RED_BUTTON_HOVER: '#8c1c03',
+    ADD_BUTTON_BG: '#19b3ab',
+    ADD_BUTTON: '#fff',
+    ADD_BUTTON_HOVER: '#328f8a'
 }
